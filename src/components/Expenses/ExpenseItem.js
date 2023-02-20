aa5d20eafe48eb376b21f9898c55ef6ac9a1818a
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-02-20 23:40:33 +0100

    add useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,28 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, {useState} from 'react';
+
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 import './ExpenseItem.css';
 
 function ExpenseItem (props) {
-	
+	// eslint-disable-next-line no-unused-vars
+	const [title, setTitle] = useState(props.title);
+
+	function clickHandler() {
+		setTitle('Updated!');
+		console.log('clicked!!');
+		
+	}
+
 	return (
 		<Card className='expense-item'>
 			<ExpenseDate date={props.date} />
 			<div className='expense-item__description'>
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className='expense-item__price'>${props.amount}</div>
+				<button onClick={clickHandler}>Change Title</button>
 			</div>
 		</Card>
 	);
