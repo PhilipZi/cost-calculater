454ec62812b0d9dcc879715ee6977d3b4f5302ee
Philip Zimmermann <philip.zimmermann@freenet.de>
2023-02-23 16:19:49 +0100

    remove test usesate and button from component expenseItem

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,7 +17,6 @@ function Expenses(props) {
 		<div>
 			<Card className='expenses'>
 				<ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-				
 				<ExpenseItem 
 					title={props.items[0].title} 
 					amount={props.items[0].amount} 
